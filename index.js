b80416c57986a3684f0dd0d0e7b6f89d8656f3e5
0penpalms <jamescartermvnyc@gmail.com>
2024-07-26 19:34:59 -0400

    All tests passing.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const countDown = function(number) {
+    while (number >= 0){
+        console.log(number);
+
+        number--;
+    } 
+};
+
+const writeCards = function(names, event_name) {
+    let message = '';
+    const cards = [];
+
+    for (let i =0; i < names.length; i++) {
+        let name = names[i];
+        message = `Thank you, ${name}, for the wonderful surprise gift!`;
+
+        cards.push(message);
+    };
+
+    return cards;
+};
